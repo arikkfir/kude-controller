950fbc80091816bf510d4d93449b33f5fae47a86
Arik Kfir <arik@kfirs.com>
2022-09-25 15:12:35 +0300

    Fix kubectlbundle args validation

--- internal/v1alpha1/kubectl_bundle.go
+++ internal/v1alpha1/kubectl_bundle.go
@@ -10,7 +10,7 @@ type KubectlBundleSpec struct {
 	Args []string `json:"args,omitempty"` // Arguments to pass to the kubectl command
 
 	// +kubebuilder:validation:Required
-	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MinItems=1
 	Files []string `json:"files,omitempty"` // Files to apply
 
 	// +kubebuilder:validation:Pattern=`^[^/]+/[^/]+$`
