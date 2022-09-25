35a4d69ced46bbf6576b1ea96b86561151e5f9b1
Arik Kfir <arik@kfirs.com>
2022-09-26 00:41:48 +0300

    Delete test bundle resources

--- scripts/delete-cluster-resources.sh
+++ scripts/delete-cluster-resources.sh
@@ -3,14 +3,12 @@
 set -exuo pipefail
 
 set +e
-for rt in GitRepository KubectlBundle CommandRun; do
+for rt in CommandRun GitRepository KubectlBundle; do
   kubectl get --namespace kude ${rt} --output=name \
     | xargs -I@ kubectl patch --namespace kude @ --type=json '--patch=[{"op":"remove","path":"/metadata/finalizers"}]'
 done
-#kubectl patch --namespace kude GitRepository --type=json '--patch=[{"op":"remove","path":"/metadata/finalizers"}]'
-#kubectl patch --namespace kude KubectlBundle --type=json '--patch=[{"op":"remove","path":"/metadata/finalizers"}]'
-#kubectl patch --namespace kude KubectlRun --type=json '--patch=[{"op":"remove","path":"/metadata/finalizers"}]'
 kustomize build test/deploy --reorder=none | kubectl delete --filename=-
+kubectl delete namespace kubectl-bundle
 set -e
 
 exit 0
