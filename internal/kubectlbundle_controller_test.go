e8a84343b1d0954c29322c973974cfc3ba54641f
Arik Kfir <arik@kfirs.com>
2022-10-25 17:16:32 +0200

    Fix KubectlBundleReconciler test

--- internal/kubectlbundle_controller_test.go
+++ internal/kubectlbundle_controller_test.go
@@ -3,45 +3,41 @@ package internal
 import (
 	"context"
 	"github.com/arikkfir/kude-controller/internal/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/utils/strings/slices"
 	"reflect"
 	"testing"
 	"time"
 )
 
-func TestCreateKubectlBundle(t *testing.T) {
+func TestKubectlBundleInitialization(t *testing.T) {
 	k8sClient, _ := setupTestEnv(t, &KubectlBundleReconciler{})
 
-	typeName := reflect.TypeOf(v1alpha1.KubectlBundle{}).Name()
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
-	defer cancel()
-
-	repo := &v1alpha1.KubectlBundle{
+	bundle := &v1alpha1.KubectlBundle{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: v1alpha1.GroupVersion.String(),
-			Kind:       typeName,
+			Kind:       reflect.TypeOf(v1alpha1.KubectlBundle{}).Name(),
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "repo1",
+			Name:      "bundle1",
 			Namespace: "default",
 		},
-		Spec: v1alpha1.KubectlBundleSpec{},
+		Spec: v1alpha1.KubectlBundleSpec{
+			DriftDetectionInterval: "5sec",
+			SourceRepository:       "ns1/repo1",
+			Files:                  []string{"*.yaml"},
+		},
 	}
-	if err := k8sClient.Create(ctx, repo); err != nil {
-		t.Errorf("failed to create %s: %v", typeName, err)
-	}
+	lookupKey := types.NamespacedName{Name: bundle.Name, Namespace: bundle.Namespace}
 
-	repoLookupKey := types.NamespacedName{Name: repo.Name, Namespace: repo.Namespace}
-	createdObject := v1alpha1.KubectlBundle{}
-	for {
-		if err := k8sClient.Get(ctx, repoLookupKey, &createdObject); err != nil {
-			t.Errorf("failed to get %s: %v", typeName, err)
-		} else if slices.Contains(createdObject.Finalizers, finalizerKubectlBundle) {
-			break
-		} else {
-			time.Sleep(1 * time.Second)
+	ctx := context.Background()
+	require.NoErrorf(t, k8sClient.Create(ctx, bundle), "resource creation failed")
+	assert.EventuallyWithTf(t, func(c *assert.CollectT) {
+		var r v1alpha1.KubectlBundle
+		if assert.NoErrorf(c, k8sClient.Get(ctx, lookupKey, &r), "resource lookup failed") {
+			assert.Contains(c, r.Finalizers, finalizerKubectlBundle, "finalizer not found")
 		}
-	}
+	}, 5*time.Second, 1*time.Second, "resource not cloned correctly")
 }
